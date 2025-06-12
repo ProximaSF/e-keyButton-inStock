5a2d1bd3cfb33f231d4a2ca2b213ed1645d704d8
ProximaSF <proximafactor@gmail.com>
2025-06-11 23:18:19 -0400

    edited main file

--- main.py
+++ main.py
@@ -8,61 +8,66 @@ import time
 
 url = "https://www.quikfixlaptopkeys.com/?product=asus-rog-flow-x16-gv601-keyboard-key-replacement-kit"
 driver = webdriver.Chrome()
-
+counter = 1
 
 def send_webhook_msg(title, msg):
     webhook_embed(title, msg)
     return
 
 def item_stock_checker():
+    global counter
     try:
-        print("Opening page")
+        #print("Opening page")
         driver.get(url)
 
-        print("Checking Page title")
+        #print("Checking Page title")
         assert "ASUS ROG Flow X16 GV601 Replacement" in driver.title
-        print("Title found")
+        #print("Title found")
 
-        print("Finding black button image")
+        #print("Finding black button image")
         black_img_button = driver.find_element(By.CSS_SELECTOR, 'div[data-value="key-color-black"]')
 
         if black_img_button:
-            print("Image Found")
+            #print("Image Found")
             black_img_button.click()
 
-            print("Finding Dropdown menu")
+            #print("Find Dropdown menu")
             drop_down_elem = driver.find_element(By.CSS_SELECTOR, 'select[data-attribute_name="attribute_pa_key"]')
             drop_down_elem.click()
 
-            print("Finding \"e\" option")
+            #print("Find \"e\" option")
             e_option_elem = driver.find_element(By.CSS_SELECTOR, 'option[value="key-e"]')
             e_option_elem.click()
             drop_down_elem.click() # close dropdown
 
-            print("Checking if item in stock")
+            #print("Check if item in stock")
             check_stock = driver.find_element(By.CSS_SELECTOR, 'button[class="single_add_to_cart_button button alt wp-element-button disabled wc-variation-is-unavailable"]')
+
             if check_stock:
-                print("Find item stock info")
+                #print("Find item stock info")
                 button_text = check_stock.text
                 print(button_text)
                 instock = True if "Out" not in button_text else False
                 if instock:
                     title = ''
-                    msg = "🟢: IN STOCK!"
+                    msg = f"{counter}🟢: IN STOCK!"
                     send_webhook_msg(title, msg)
                 else:
                     title = ''
-                    msg = "🔴: Not in stock"
+                    msg = f"{counter}🔴: Not in stock"
                     send_webhook_msg(title, msg)
+
+                counter += 1
         else:
             print("Image not found")
     except Exception as e:
         print("/n/n")
-        print(e)
-
+        send_webhook_msg("**CODE ERROR**", str(e))
+        exit()
 
 def main():
-    schedule.every(10).minute.do(item_stock_checker)
+    schedule.every(10).minutes.do(item_stock_checker)
+    item_stock_checker()
     while True:
         schedule.run_pending()
         time.sleep(1)
